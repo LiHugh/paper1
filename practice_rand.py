46f07791b2cbff2a9363c2fea43e3eb40b756eea
LiHugh <37071490+LiHugh@users.noreply.github.com>
2022-05-13 23:43:45 +0800

    Delete practice_rand.py

--- practice_rand.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-size = 15
-K = 5
-for t in range(size):
-    for k in range(K):
-        future = np.random.randint(t, size)
-        print('-------------', t)
-        print(future)
